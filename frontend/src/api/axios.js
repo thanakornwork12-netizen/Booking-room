2018b7ec721755615b8ad4203757c80a08a8b9c2
Thanakorn <thanakornwork12@gmail.com>
2026-03-11 13:44:39 +0700

    draft2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://room-booking-v774.onrender.com/api',
 })
-
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('access_token')
   if (token) config.headers.Authorization = `Bearer ${token}`
