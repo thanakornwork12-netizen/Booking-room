97bd25ecdfcf30115882746db31fd349f54cbd47
Thanakorn <thanakornwork12@gmail.com>
2026-03-11 13:26:19 +0700

    add render deploy config

--- room_booking/settings.py
+++ room_booking/settings.py
@@ -141,3 +141,13 @@ SIMPLE_JWT = {
 }
 
 
+import os
+import dj_database_url
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+
+ALLOWED_HOSTS = ['*']
